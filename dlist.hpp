2afdeeb68a612f8fd0f8da78c1a085bbd169335c
AslikyanZhora <43322906+AslikyanZhora@users.noreply.github.com>
2019-04-03 20:28:13 +0400

    Update dlist.hpp

--- dlist.hpp
+++ dlist.hpp
@@ -11,12 +11,11 @@ public:
 	void makeEmpty();
 	void push_front(const T& val);
 	void push_back(const T& val);
-	void insert(const size_t& pos, const T& val);
 	void pop_front();
 	void pop_back();
-	void erase(const size_t pos);
+
 	const T& get(const size_t& pos);
-//private:
+private:
 	struct Node
 	{
 		Node(const T& val = 0) :next(nullptr), prev(nullptr), value(val){}
